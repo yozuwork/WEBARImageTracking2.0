3f1a19ef05bfc553c284a7b8e31b185cb8f842aa
yozu <yozuwork@gmail.com>
2024-07-19 14:31:37 +0800

    feat: Update AR tracking URLs for image markers

--- assets/events.js
+++ assets/events.js
@@ -1,29 +1,35 @@
-var step = 1;
-var is_notified = false;
-var arbox = document.querySelector('.arbox');
-AFRAME.registerComponent('image-tracker-1', {
-    init: function () {
-        console.log("image-tracker-1 init");
-        arbox.style.display = "block";
-    },
-    tick: function() {
-        if (this.el.object3D.visible == true) {
-            // 掃描到辨識點
-            arbox.style.display = "block";
-            step = 2;
-        }
-    }
-});
-AFRAME.registerComponent('image-tracker-2', {
-    init: function () {
-        console.log("image-tracker-2 init");
-    },
-    tick: function() {
-        if (this.el.object3D.visible == true) {
-            if (step != 2 && !is_notified) {
-                is_notified = true;
-                alert("請先掃描封面辨識點");
+$( document ).ready(function() {
+    var step = 1;
+    var is_notified = false;
+    var arbox = document.querySelector('.arbox');
+    arbox.style.display = "block";
+    AFRAME.registerComponent('image-tracker-1', {
+        init: function () {
+            console.log("image-tracker-1 init");
+            arbox.style.display = "blcok";
+        },
+        tick: function() {
+            if (this.el.object3D.visible == true) {
+                // 掃描到辨識點
+                
+                step = 2;
             }
         }
-    }
+    });
+    AFRAME.registerComponent('image-tracker-2', {
+        init: function () {
+            console.log("image-tracker-2 init");
+        },
+        tick: function() {
+            if (this.el.object3D.visible == true) {
+                if (step != 2 && !is_notified) {
+                    is_notified = true;
+                    alert("請先掃描封面辨識點");
+                }
+            }
+        }
+    });
 });
+
+
+
