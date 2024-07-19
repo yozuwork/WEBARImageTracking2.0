19b3c0b99651b33db5f38275d9a8e705b2ee71da
yozu <yozuwork@gmail.com>
2024-07-19 15:04:43 +0800

    Update AR tracking URLs for image markers and enable AR box display when image marker is detected

--- assets/events.js
+++ assets/events.js
@@ -1,40 +1,31 @@
-var step = 1;
-var isNotified = false;
-var arbox = document.querySelector('.arbox');
-
-AFRAME.registerComponent('image-tracker-1', {
-    init: function () {
-        console.log("image-tracker-1 init");
-        alert("已進入AR模式，請掃描封面辨識點");
-    },
-    tick: function() {
-        if (this.el.object3D.visible && step === 1) {
-            arbox.style.display = "block";
-            if (!isNotified) {
+    var step = 1;
+    var is_notified = false;
+    var arbox = document.querySelector('.arbox');
+    
+    AFRAME.registerComponent('image-tracker-1', {
+        init: function () {
+            console.log("image-tracker-1 init");
+            alert("辨識成功");
+        },
+        tick: function() {
+            if (this.el.object3D.visible == true) {
+                // 掃描到辨識點
+                arbox.style.display = "block";
                 alert("辨識成功");
-                isNotified = true;
+                step = 2;
             }
-            step = 2;
         }
-    }
-});
-
-AFRAME.registerComponent('image-tracker-2', {
-    init: function () {
-        console.log("image-tracker-2 init");
-    },
-    tick: function() {
-        if (this.el.object3D.visible) {
-            if (step !== 2 && !isNotified) {
-                isNotified = true;
-                alert("請先掃描封面辨識點");
+    });
+    AFRAME.registerComponent('image-tracker-2', {
+        init: function () {
+            console.log("image-tracker-2 init");
+        },
+        tick: function() {
+            if (this.el.object3D.visible == true) {
+                if (step != 2 && !is_notified) {
+                    is_notified = true;
+                    alert("請先掃描封面辨識點");
+                }
             }
-            if (step === 2) {
-                // 这里可以添加第二个标记被识别后的逻辑
-                console.log("第二个标记被识别");
-            }
-        } else {
-            isNotified = false;
         }
-    }
-});
+    });
